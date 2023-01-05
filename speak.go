b09afcc75d35bb6f2d95f31579c96a9198ba010d
Turbonator0 <78885232+Turbonator0@users.noreply.github.com>
2023-01-05 08:41:06 +0200

    Make rufus a possible string and fix casing in speak

--- speak.go
+++ speak.go
@@ -15,9 +15,9 @@ func randomNumber(max int) int {
 func marshal(a interface{}) string {
 	marshalled, err := json.Marshal(a)
 	check(err)
-	var returnstring string
-	check(json.Unmarshal(marshalled, &returnstring))
-	return returnstring
+	var returnString string
+	check(json.Unmarshal(marshalled, &returnString))
+	return returnString
 }
 
 func check(e error) {
@@ -27,15 +27,15 @@ func check(e error) {
 }
 
 func speak(count int) []string {
-	const LASTINDEX int = 711
 	var ReturnList []string
 	content, err := os.ReadFile("words.json")
 	check(err)
 	var unmarshalled []interface{}
 	err = json.Unmarshal(content, &unmarshalled)
 	check(err)
+	lastIndex := len(unmarshalled)
 	for i := 1; i <= count; i++ {
-		item := unmarshalled[randomNumber(LASTINDEX)]
+		item := unmarshalled[randomNumber(lastIndex)]
 		ReturnList = append(ReturnList, marshal(item))
 	}
 	return ReturnList
